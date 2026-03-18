baccf4eb3efeec398310a413168b84170e841079
LCurcuma <zhihnadya2002@gmail.com>
2026-03-18 08:02:53 +0100

    added font to footer

--- portfolio/components/footer/Footer.jsx
+++ portfolio/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Footer() {
   return (
     <footer>
-      <h2 className={styles.h2_footer}>Contact me</h2>
+      <h2 className={`${styles.h2_footer} introBig`}>Contact me</h2>
       <div className={styles.icon_container}>
         <div className={styles.icon}></div>
         <div className={styles.icon}></div>

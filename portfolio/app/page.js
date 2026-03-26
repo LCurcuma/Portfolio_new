7204db775cda23e8e65be92d492e52e1bc1db476
LCurcuma <zhihnadya2002@gmail.com>
2026-03-26 15:22:36 +0100

    added redirect to /en page from / page

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,18 +1,5 @@
-import styles from "./page.module.css";
-import Header from "@/components/header/Header";
-import Hero from "@/components/hero/Hero";
-import Project from "@/components/projects/Projects";
-import Footer from "@/components/footer/Footer";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <Hero />
-        <Project />
-      </main>
-      <Footer />
-    </div>
-  );
+  redirect("/en");
 }

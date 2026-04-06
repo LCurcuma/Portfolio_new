5d58c7fb0931ef991eda2ff9f2af0ebd0c8ae5db
LCurcuma <zhihnadya2002@gmail.com>
2026-04-06 18:28:38 +0200

    added translation for alt image, edited styles of about_me-section and added icons

--- portfolio/components/hero/Hero.jsx
+++ portfolio/components/hero/Hero.jsx
@@ -6,7 +6,11 @@ export default function Hero({ translation }) {
     <>
       {/*Hero section*/}
       <section className={styles.hero_section}>
-        <img src="/images/nadiia.png" alt="" className={styles.hero_image}/>
+        <img
+          src="/images/nadiia.png"
+          alt={`${translation.hero_image_alt}`}
+          className={styles.hero_image}
+        />
         <h1 className={`${styles.h1} introBig`}>
           {translation.hero.h1_1}
           <br />
@@ -25,30 +29,49 @@ export default function Hero({ translation }) {
       <section className={styles.about_sec} id="about_me">
         <div className={styles.about_div}>
           <div className={styles.flex_column_div}>
-            <h2 className="introBig">About me</h2>
-            <p></p>
+            <h2 className={`${styles.h2} introBig`}>About me</h2>
+            <p className={`${styles.p} introMain`}>
+              I'm a person, who enjoys combining art with code to create cute
+              interactive websites. I draw components for websites, stickers,
+              animations and illustrations. <br />
+              <br />
+              Currently I'm studying at Zealand Academy as multimedia designer
+              and drawing and making websites, when I have free time.
+            </p>
             <img src="" />
           </div>
 
           {/*Skills*/}
           <div className={styles.flex_column_div}>
-            <h2 className="introBig">Skills</h2>
+            <h2 className={`${styles.h2} introBig`}>Skills</h2>
 
             {/*Design and multimedia*/}
             <div className={styles.flex_column_div}>
-              <h3 className="introBig">Design and multimedia</h3>
+              <h3 className={`${styles.h3} introBig`}>Design and multimedia</h3>
               {/*Icons*/}
               <div className={styles.icons}>
-                <img src="" alt="" className={styles.icon} />
-                <img src="" alt="" className={styles.icon} />
-                <img src="" alt="" className={styles.icon} />
+                <img
+                  src="/images/design_icons/clip_studio_paint_icon.png"
+                  alt="Clip Studio Paint icon"
+                  className={styles.icon}
+                />
+                <img
+                  src="/images/design_icons/adobe_photoshop_icon.png"
+                  alt=""
+                  className={styles.icon}
+                />
+                <img
+                  src="/images/design_icons/adobe_illustrator_icon.png"
+                  alt=""
+                  className={styles.icon}
+                />
                 <img src="" alt="" className={styles.icon} />
               </div>
             </div>
 
             {/*Programming*/}
             <div className={styles.flex_column_div}>
-              <h3 className="introBig">Frontend-development</h3>
+              <h3 className={`${styles.h3} introBig`}>Frontend-development</h3>
 
               {/*Icons*/}
               <div className={styles.icons}>

3c7d0c63ea52ec5829ed12da7371cc4cc3cc2140
LCurcuma <zhihnadya2002@gmail.com>
2026-05-03 18:35:55 +0200

    added the canva logo

--- portfolio/components/hero/Hero.jsx
+++ portfolio/components/hero/Hero.jsx
@@ -145,7 +145,19 @@ export default function Hero({ translation, links }) {
                     {translation.about_me_section.skills.p_4}
                   </p>
                 </div>
-                {/*+ Canva, + Corel Draw, + Adobe InDesign, + Paint lol*/}
+                <div className={styles.icon_div}>
+                  <img
+                    src={links.images.icons.canva}
+                    alt={translation.about_me_section.skills.img_alt_5}
+                    className={`${styles.icon} scroll_show_animate`}
+                  />
+                  <p
+                    className={`${styles.p_icon} introMain scroll_show_animate`}
+                  >
+                    {translation.about_me_section.skills.p_5}
+                  </p>
+                </div>
+                {/*+ Corel Draw, + Adobe InDesign, + Paint lol*/}
               </div>
             </div>
 

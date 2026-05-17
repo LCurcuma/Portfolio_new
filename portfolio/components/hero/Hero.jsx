014255c03e1e444f2ffbc4ca6d432e7f024cf683
LCurcuma <zhihnadya2002@gmail.com>
2026-05-17 18:04:32 +0200

    added scss icon

--- portfolio/components/hero/Hero.jsx
+++ portfolio/components/hero/Hero.jsx
@@ -231,14 +231,14 @@ export default function Hero({ translation, links }) {
                 </div>
                 <div className={styles.icon_div}>
                   <img
-                    src=""
-                    alt=""
+                    src={links.images.icons.scss}
+                    alt={translation.about_me_section.skills.img_alt_12}
                     className={`${styles.icon} scroll_show_animate`}
                   />
                   <p
                     className={`${styles.p_icon} introMain scroll_show_animate`}
                   >
-                    100%
+                    {translation.about_me_section.skills.p_12}
                   </p>
                 </div>
               </div>

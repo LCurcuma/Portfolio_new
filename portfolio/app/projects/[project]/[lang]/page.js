d403749196ad65229d53c627f2a66efd1db63b28
LCurcuma <zhihnadya2002@gmail.com>
2026-06-23 19:23:22 +0200

    projects pages init

--- portfolio/app/projects/[project]/[lang]/page.js
+++ portfolio/app/projects/[project]/[lang]/page.js
@@ -1,8 +1,20 @@
 "use client";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function Projects({params}) {
+export default function Projects() {
+    const [lang, setLang] = useState("");
+    const [projects, setProjects] = useState("");
+    const params = useParams();
+    
+    useEffect(() => {
+        setLang(params.lang);
+        setProjects(params.project);
+    },[])
+
+    console.log(lang, projects);
     return (
-        <></>
+        <>
+        </>
     )
 }

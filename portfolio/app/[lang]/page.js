3dae8654f5198b1c532ca30c29f514f3299e66ab
LCurcuma <zhihnadya2002@gmail.com>
2026-06-25 16:54:31 +0200

    added footer

--- portfolio/app/[lang]/page.js
+++ portfolio/app/[lang]/page.js
@@ -8,6 +8,7 @@ import translationUA from "@/components/translations/TranslationUA.json";
 import links from "@/components/translations/links.json";
 import Hero from "@/components/hero/Hero";
 import Project from "@/components/projects/Projects";
+import Footer from "@/components/footer/Footer";
 
 export default function MainPage() {
   const [lang, setLang] = useState("");
@@ -22,7 +23,8 @@ export default function MainPage() {
       <>
         <Header translation={translationEN} />
         <Hero translation={translationEN} links={links} />
-        <Project translation={translationEN} links={links}/>
+        <Project translation={translationEN} links={links} />
+        <Footer translation={translationEN}/>
       </>
     );
   } else if (lang === "dk") {
@@ -31,6 +33,7 @@ export default function MainPage() {
         <Header translation={translationDK} />
         <Hero translation={translationDK} links={links} />
         <Project translation={translationDK} links={links} />
+        <Footer translation={translationDK} />
       </>
     );
   } else {
@@ -39,6 +42,7 @@ export default function MainPage() {
         <Header translation={translationUA} />
         <Hero translation={translationUA} links={links} />
         <Project translation={translationUA} links={links} />
+        <Footer translation={translationUA} />
       </>
     );
   }
--- portfolio/components/footer/Footer.jsx
+++ portfolio/components/footer/Footer.jsx
@@ -1,17 +1,11 @@
+"use client";
+
 import styles from "./page.module.css";
 
-export default function Footer() {
+export default function Footer({ translation }) {
   return (
     <footer>
       <h2 className={`${styles.h2_footer} introBig`}>Contact me</h2>
-      <div className={styles.icon_container}>
-        <div className={styles.icon}></div>
-        <div className={styles.icon}></div>
-        <div className={styles.icon}></div>
-        <div className={styles.icon}></div>
-        <div className={styles.icon}></div>
-        <div className={styles.icon}></div>
-      </div>
     </footer>
   );
 }

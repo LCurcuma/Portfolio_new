6721cb97fa665ca57a6d10947e63fabcfb2ce9e5
LCurcuma <zhihnadya2002@gmail.com>
2026-06-28 18:47:17 +0200

    styled the carousel a little

--- portfolio/components/projects/Projects.jsx
+++ portfolio/components/projects/Projects.jsx
@@ -14,83 +14,6 @@ export default function Project({ translation, links }) {
         {translation.projects_section.h2}
       </h2>
       <section className={`${styles.projects} scroll_show_animate`}>
-        <div className={styles.websites}>
-          <h2 className={`${styles.h3} ${styles.pink} introBig`}>
-            {translation.projects_section.websites.h3}
-          </h2>
-          <div className={styles.imageContainer}>
-            {links.images.projects.websites.map((link, index) => (
-              <a className={styles.imgCont} key={index}>
-                <p className={`${styles.orange} introMain ${styles.p}`}>
-                  {translation.projects_section.websites.ps[index]}
-                </p>
-                <img
-                  src={link}
-                  className={`${styles.projectImage} ${styles.withBR} ${styles.zoom}`}
-                  alt={translation.projects_section.websites.alts[index]}
-                />
-              </a>
-            ))}
-          </div>
-        </div>
-        <div className={styles.logos}>
-          <h2 className={`${styles.h3} ${styles.pink} introBig`}>
-            {translation.projects_section.logos.h3}
-          </h2>
-          <div className={styles.imageContainer}>
-            {links.images.projects.logos.map((link, index) => (
-              <a className={styles.imgCont} key={index}>
-                <p className={`${styles.orange} introMain ${styles.p}`}>
-                  {translation.projects_section.logos.ps[index]}
-                </p>
-                <img
-                  src={link}
-                  className={`${styles.projectImage} ${styles.zoom}`}
-                  alt={translation.projects_section.logos.alts[index]}
-                />
-              </a>
-            ))}
-          </div>
-        </div>
-        <div className={styles.artworks}>
-          <h2 className={`${styles.h3} ${styles.pink} introBig`}>
-            {translation.projects_section.artworks.h3}
-          </h2>
-          <div className={styles.imageContainer}>
-            {links.images.projects.artworks.map((link, index) => (
-              <a className={styles.imgCont} key={index}>
-                <p className={`${styles.orange} introMain ${styles.p}`}>
-                  {translation.projects_section.artworks.ps[index]}
-                </p>
-                <img
-                  src={link}
-                  className={`${styles.projectImage} ${styles.withBR} ${styles.zoom}`}
-                  alt={translation.projects_section.artworks.alts[index]}
-                />
-              </a>
-            ))}
-          </div>
-        </div>
-        <div className={styles.animations}>
-          <h2 className={`${styles.h3} ${styles.pink} introBig`}>
-            {translation.projects_section.animations.h3}
-          </h2>
-          <div className={styles.imageContainer}>
-            {links.images.projects.animations.map((link, index) => (
-              <a className={styles.imgCont} key={index}>
-                <p className={`${styles.orange} introMain ${styles.p}`}>
-                  {translation.projects_section.animations.ps[index]}
-                </p>
-                <img
-                  src={link}
-                  className={`${styles.projectImage} ${styles.withBR} ${styles.zoom}`}
-                  alt={translation.projects_section.animations.alts[index]}
-                />
-              </a>
-            ))}
-          </div>
-        </div>
-      </section>
       <div className={`${styles.projectsCarousel}`}>
         <div className={`${styles.image}`}>
           <img src={projectURL} className={`${styles.projectBigImg}`} />
@@ -109,10 +32,6 @@ export default function Project({ translation, links }) {
             src="/images/projects/websites/first_help_experts_screenshot.png"
             className={styles.projectImg}
           />
-          <p
-            className={`${styles.button_text}`}
-            styles={{ display: "none" }}
-          ></p>
         </div>
         <div
           className={`${styles.button} ${projectURL !== "/images/projects/websites/waybly_screenshot.png" && styles.unchecked}`}
@@ -124,12 +43,9 @@ export default function Project({ translation, links }) {
             src="/images/projects/websites/waybly_screenshot.png"
             className={styles.projectImg}
           />
-          <p
-            className={`${styles.button_text}`}
-            styles={{ display: "none" }}
-          ></p>
         </div>
       </div>
+      </section>
     </>
   );
 }

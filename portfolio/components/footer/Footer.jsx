bacdd00d32a3c8718570c44abbb54a901d078bca
LCurcuma <zhihnadya2002@gmail.com>
2026-07-03 11:58:36 +0200

    added footer

--- portfolio/components/footer/Footer.jsx
+++ portfolio/components/footer/Footer.jsx
@@ -4,16 +4,19 @@ import styles from "./page.module.css";
 
 export default function Footer({ translation, links }) {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <div className={styles.rel}>
         <h2 className={`${styles.h2_footer} introBig`}>
           {translation.footer.h2}
         </h2>
         <img src="/images/Nddd_hello.png" className={styles.Nddd_hi} />
         <div className={styles.links}>
-          <a href="https://mail.google.com/mail/u/0/?fs=1&to=lumilicurcuma@gmail.com&tf=cm">
-            <img src=""/>
-          </a>
+            {links.images.icons.footer.map((icon, id) => (
+              <a key={id} href={links.links.contact[id]} className={styles.contact_link}>
+                <img src={icon} className={styles.contact_img} />
+                <p className={`${styles.link_text} introMain`}>{links.links.contact_text[id]}</p>
+              </a>
+            ))}
         </div>
       </div>
     </footer>

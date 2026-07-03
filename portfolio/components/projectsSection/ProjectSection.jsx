506400fd69d285f4efb55c90b890ea2ea7180103
LCurcuma <zhihnadya2002@gmail.com>
2026-07-03 16:54:06 +0200

    added project section/tile

--- portfolio/components/projectsSection/ProjectSection.jsx
+++ portfolio/components/projectsSection/ProjectSection.jsx
@@ -1,7 +1,28 @@
+import styles from "./page.module.css";
+
 export default function ProjectSection() {
   return (
-    <>
-      <img />
-    </>
+    <section className={styles.projects_section}>
+      <h1 className={`${styles.h1} introBig`}>Websites</h1>
+      <div className={styles.projects_container}>
+        <div className={styles.project_tile}>
+          <img
+            src="/images/projects/websites/first_help_experts_screenshot.png"
+            className={styles.project_image}
+          />
+          <div>
+            <h2 className={`${styles.h2} introBig`}>First Help Experts</h2>
+            <p className={`${styles.text} introMain`}>
+              Do you want to learn how to do first help engaging and playful
+              way? The app "Førstehjælpseksperten" is made for those, who need
+              to go through first help courses or to refresh the knowledge.
+            </p>
+            <a href="" className={styles.more}>
+              More about project
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }

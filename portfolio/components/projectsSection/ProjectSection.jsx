4ed6ad548812a1692545f257cea5abd9ab28d63f
LCurcuma <zhihnadya2002@gmail.com>
2026-07-04 17:40:59 +0200

    added loop to create cards

--- portfolio/components/projectsSection/ProjectSection.jsx
+++ portfolio/components/projectsSection/ProjectSection.jsx
@@ -1,30 +1,39 @@
 import styles from "./page.module.css";
 
-export default function ProjectSection({translation, links}) {
+export default function ProjectSection({ translation, links }) {
   return (
     <section className={styles.projects_section}>
       <h1 className={`${styles.h1} introBig`}>Websites</h1>
       <div className={styles.projects_container}>
-        {}
-        <div className={`${styles.project_tile} ${styles.row}`}>
-          <img
-            src="/images/projects/websites/first_help_experts_screenshot.png"
-            className={styles.project_image}
-          />
-          <div>
-            <h2 className={`${styles.h2} introBig`}>
-              First Help Experts <span className={styles.date}>May 2026 - June 2026</span>
-            </h2>
-            <p className={`${styles.text} introMain`}>
-              Do you want to learn how to do first help engaging and playful
-              way? The app "Førstehjælpseksperten" is made for those, who need
-              to go through first help courses or to refresh the knowledge.
-            </p>
-            <a href="" className={styles.more}>
-              More about project
-            </a>
+        {translation.projects.h2.map((project, id) => (
+          <div
+            key={id}
+            className={`${styles.project_tile} ${id % 2 === 0 ? styles.row_reverse : styles.row}`}
+          >
+            <img
+              src="/images/projects/websites/first_help_experts_screenshot.png"
+              className={styles.project_image}
+              alt={translation.projects.alt[id]}
+            />
+            <div>
+              <h2 className={`${styles.h2} introBig`}>
+                {project}
+                <span className={styles.date}>
+                  {translation.projects.dato[id]}
+                </span>
+              </h2>
+              <p className={`${styles.text} introMain`}>
+                {translation.projects.p[id]}
+              </p>
+              <p className={`${styles.text} introMain`}>
+                {translation.projects.p2[id]}
+              </p>
+              <a href={translation.projects.href[id]} className={styles.more}>
+                More about project
+              </a>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );

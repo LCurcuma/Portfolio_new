0d26b83392096d1158e138ce5ae985f5c31926d2
LCurcuma <zhihnadya2002@gmail.com>
2026-07-08 19:51:30 +0200

    added basic text-content to the images section

--- portfolio/components/imagesSection/ImagesSection.jsx
+++ portfolio/components/imagesSection/ImagesSection.jsx
@@ -1,9 +1,26 @@
 import styles from "./page.module.css";
 
 export default function ImagesSection({ translation, links }) {
-    return (
-        <section>
-            <h1>Illustrations</h1>
-        </section>
-    )
+  return (
+    <section>
+      <h2>Illustrations</h2>
+      <div>
+        <img src="/images/projects/illustrations/fmb.png" />
+        <img src="/images/projects/illustrations/ynyn.png" />
+        <img src="/images/projects/illustrations/mhndfornyunderthechristmastree.png" />
+      </div>
+      <h2>Animations</h2>
+      <div>
+        <img src="/images/projects/animations/manya_fail_f.gif" />
+        <img src="/images/projects/animations/manya_sit_f.gif" />
+        <img src="/images/projects/animations/manya_stand_а.gif" />
+      </div>
+      <h2>Logos</h2>
+      <div>
+        <img src="/images/projects/logos/first_help_experts_full_logo.png" />
+        <img src="/images/projects/logos/no_limitation_logo.png" />
+        <img src="/images/projects/logos/waybly_logo.png" />
+      </div>
+    </section>
+  );
 }

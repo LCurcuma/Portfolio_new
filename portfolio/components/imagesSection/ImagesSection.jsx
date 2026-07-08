691cea6b6b2cf57aa19cb8fc8587c7e245ea9773
LCurcuma <zhihnadya2002@gmail.com>
2026-07-08 20:34:33 +0200

    added styles to the h2

--- portfolio/components/imagesSection/ImagesSection.jsx
+++ portfolio/components/imagesSection/ImagesSection.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function ImagesSection({ translation, links }) {
   return (
     <section className={styles.image_section}>
-      <h2>Illustrations</h2>
+      <h2 className={`${styles.h2} introBig`}>Illustrations</h2>
       <div>
         <img src="/images/projects/illustrations/fmb.png" />
         <img src="/images/projects/illustrations/ynyn.png" />

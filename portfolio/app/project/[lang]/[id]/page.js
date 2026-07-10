d5bfa845ba1f3e8f74986fea908b5c98010b1b56
LCurcuma <zhihnadya2002@gmail.com>
2026-07-10 18:42:41 +0200

    added useEffect

--- portfolio/app/project/[lang]/[id]/page.js
+++ portfolio/app/project/[lang]/[id]/page.js
@@ -3,11 +3,14 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function projectPage() {
-    const [projectId, setProjectId] = useState();
-    const [lang, setLang] = useState("");
-    const params = useParams();
+  const [projectId, setProjectId] = useState();
+  const [lang, setLang] = useState("");
+  const params = useParams();
 
-    return (
-        <></>
-    )
+  useEffect(() => {
+    setLang(params.lang);
+    setProjectId(params.id);
+  }, []);
+
+  return <></>;
 }

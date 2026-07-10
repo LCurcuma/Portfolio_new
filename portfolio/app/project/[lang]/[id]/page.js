4e5ae0ea224b4b60dcdc84c2e6e2688052929937
LCurcuma <zhihnadya2002@gmail.com>
2026-07-10 18:40:08 +0200

    added variables

--- portfolio/app/project/[lang]/[id]/page.js
+++ portfolio/app/project/[lang]/[id]/page.js
@@ -3,6 +3,10 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function projectPage() {
+    const [projectId, setProjectId] = useState();
+    const [lang, setLang] = useState("");
+    const params = useParams();
+
     return (
         <></>
     )

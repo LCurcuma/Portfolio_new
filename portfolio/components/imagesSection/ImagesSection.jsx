c761352cfd5ae27959c29ab5cb92af7d6f32365f
LCurcuma <zhihnadya2002@gmail.com>
2026-07-10 18:26:04 +0200

    added link to the images

--- portfolio/components/imagesSection/ImagesSection.jsx
+++ portfolio/components/imagesSection/ImagesSection.jsx
@@ -8,12 +8,13 @@ export default function ImagesSection({ translation, links }) {
           <h2 className={`${styles.h2} introBig`}>{section.h2}</h2>
           <div className={styles.images_container}>
             {links.images_section[id].images.map((src, imgId) => (
+              <a href={section.links[imgId]} key={imgId}>
               <img
-                key={imgId}
                 src={src}
                 className={styles.image}
                 alt={section.alt[imgId]}
-              />
+                />
+                </a>
             ))}
           </div>
         </div>

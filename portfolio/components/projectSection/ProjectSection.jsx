7ffdc6b1d355dd0797313c06688d66733010ea88
LCurcuma <zhihnadya2002@gmail.com>
2026-07-12 13:14:24 +0200

    added test content

--- portfolio/components/projectSection/ProjectSection.jsx
+++ portfolio/components/projectSection/ProjectSection.jsx
@@ -2,5 +2,52 @@
 import styles from "./page.module.css";
 
 export default function ProjectSection({ translation, links, id }) {
-  return <></>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Name of project</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa,
+            molestias ipsam accusantium doloremque, ipsum odit aliquid tempora
+            voluptas soluta officia pariatur dignissimos totam consequatur omnis
+            repellat eos! Tempore, iste modi!
+          </p>
+        </div>
+        <img src="/images/projects/animations/manya_fail_f.gif" />
+      </div>
+      <div>
+        <img src="/images/projects/animations/manya_fail_f.gif" />
+        <div>
+          <h2>Process section</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit laudantium libero perferendis accusamus facilis ab id,
+            odio inventore animi esse. Corrupti eaque quasi veritatis recusandae
+            consectetur nemo iure dolor magni.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Process section</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Necessitatibus iure quisquam porro ducimus perferendis nemo, autem
+            eos inventore dolore fugit esse ratione iusto. Tenetur autem quam
+            soluta iusto temporibus error.
+          </p>
+        </div>
+        <div>
+          <h2>Process section</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A vero
+            ducimus, tempora magnam blanditiis iusto pariatur porro, incidunt
+            libero non ullam asperiores aliquam velit modi culpa ipsum eveniet
+            voluptatibus consequatur?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }

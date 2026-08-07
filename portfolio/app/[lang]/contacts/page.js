574d4159636e61490689166528d5514732c29215
LCurcuma <zhihnadya2002@gmail.com>
2026-08-07 16:52:55 +0200

    added observer to the contact page

--- portfolio/app/[lang]/contacts/page.js
+++ portfolio/app/[lang]/contacts/page.js
@@ -14,24 +14,55 @@ export default function ContactsPage() {
   useEffect(() => {
     setLang(l.lang);
   }, [l]);
-    
-    if (lang === "en") {
-        return (
-            <>
-                <Header translation={translationEN}/>
-            </>
-        )
-    } else if (lang === "dk") {
-        return (
-          <>
-            <Header translation={translationDK} />
-          </>
-        );
-    } else {
-        return (
-          <>
-            <Header translation={translationUA} />
-          </>
-        );
-    }
+
+  const [clickedMore, setClickedMore] = useState(false);
+
+  useEffect(() => {
+    const elements = document.querySelectorAll(`.scroll_show_animate`);
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          } else {
+            entry.target.classList.remove("show");
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      },
+    );
+
+    elements.forEach((el) => {
+      observer.observe(el);
+
+      if (el.getBoundingClientRect().top < window.innerHeight) {
+        el.classList.add("show");
+      }
+    });
+
+    return () => observer.disconnect();
+  }, [clickedMore]);
+
+  if (lang === "en") {
+    return (
+      <>
+        <Header translation={translationEN} />
+      </>
+    );
+  } else if (lang === "dk") {
+    return (
+      <>
+        <Header translation={translationDK} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Header translation={translationUA} />
+      </>
+    );
+  }
 }

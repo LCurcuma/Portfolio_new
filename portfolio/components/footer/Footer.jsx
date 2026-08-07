a5cce1ddff034102e083cb55925b82e3f1244c3c
LCurcuma <zhihnadya2002@gmail.com>
2026-08-07 16:50:54 +0200

    edited footer of the page

--- portfolio/components/footer/Footer.jsx
+++ portfolio/components/footer/Footer.jsx
@@ -6,24 +6,12 @@ export default function Footer({ translation, links }) {
   return (
     <footer className={`${styles.footer} scroll_show_animate`}>
       <div className={styles.rel}>
+        <a href={translation.footer.link} className={styles.link}>
         <h2 className={`${styles.h2_footer} introBig`}>
           {translation.footer.h2}
-        </h2>
+          </h2>
+          </a>
         <img src="/images/Nddd_hello.png" className={styles.Nddd_hi} />
-        <div className={styles.links}>
-          {links.images.icons.footer.map((icon, id) => (
-            <a
-              key={id}
-              href={links.links.contact[id]}
-              className={styles.contact_link}
-            >
-              <img src={icon} className={styles.contact_img} />
-              <p className={`${styles.link_text} introMain`}>
-                {links.links.contact_text[id]}
-              </p>
-            </a>
-          ))}
-        </div>
       </div>
     </footer>
   );

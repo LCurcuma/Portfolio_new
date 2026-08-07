bf47bb551d2303fec4261c56b52f11dbe87dc04e
LCurcuma <zhihnadya2002@gmail.com>
2026-08-07 16:42:32 +0200

    added contact page

---
+++ portfolio/app/[lang]/contacts/page.js
@@ -0,0 +1,37 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import Header from "@/components/header/Header";
+import translationEN from "@/components/translations/TranslationEN.json";
+import translationDK from "@/components/translations/TranslationDK.json";
+import translationUA from "@/components/translations/TranslationUA.json";
+import links from "@/components/translations/links.json";
+
+export default function ContactsPage() {
+  const [lang, setLang] = useState("");
+  const l = useParams();
+
+  useEffect(() => {
+    setLang(l.lang);
+  }, [l]);
+    
+    if (lang === "en") {
+        return (
+            <>
+                <Header translation={translationEN}/>
+            </>
+        )
+    } else if (lang === "dk") {
+        return (
+          <>
+            <Header translation={translationDK} />
+          </>
+        );
+    } else {
+        return (
+          <>
+            <Header translation={translationUA} />
+          </>
+        );
+    }
+}

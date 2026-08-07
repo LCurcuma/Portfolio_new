0ba1f10092df2e61c9a010ea4e1a94120b989bef
LCurcuma <zhihnadya2002@gmail.com>
2026-08-07 17:47:27 +0200

    added styles + edited links to the contact page

--- portfolio/components/contactsSection/ContactsSection.jsx
+++ portfolio/components/contactsSection/ContactsSection.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import styles from "./page.module.css";
 
 export default function ContactsSection({ translation, links }) {
       const [clickedMore, setClickedMore] = useState(false);
@@ -34,41 +35,43 @@ export default function ContactsSection({ translation, links }) {
       }, [clickedMore]);
 
     return (
-      <div>
-        <div>
-          <h2>Contacts</h2>
-          <p>
+      <div className={styles.contactDiv}>
+        <div className={styles.container}>
+          <h2 className={`${styles.h2} introBig`}>Contacts</h2>
+          <p className={`${styles.p} introMain`}>
             Feel free to get in touch if you'd like to discuss a project,
             collaboration, or have any questions about my work. I'm most active
             on:
           </p>
-          <ul>
-            <a href="">
-              <li>
+          <ul className={styles.ul}>
+            <a href="" className={styles.link}>
+              <li className={styles.list}>
                 <img src="" />
                 <p>zhihnadya2002@gmail.com</p>
               </li>
             </a>
-            <a href="">
-              <li>
+            <a href="" className={styles.link}>
+              <li className={styles.list}>
                 <img src="" />
                 <p>zhihnadya2002@gmail.com</p>
               </li>
             </a>
           </ul>
         </div>
-        <div>
-          <h2>Social media</h2>
-          <p>Here you can see more my works, sketches, animations etc.</p>
-          <ul>
-            <a href="">
-              <li>
+        <div className={styles.container}>
+          <h2 className={`${styles.h2} introBig`}>Social media</h2>
+          <p className={`${styles.p} introMain`}>
+            Here you can see more my works, sketches, animations etc.
+          </p>
+          <ul className={styles.ul}>
+            <a href="" className={styles.link}>
+              <li className={styles.list}>
                 <img src="" />
                 <p>zhihnadya2002@gmail.com</p>
               </li>
             </a>
-            <a href="">
-              <li>
+            <a href="" className={styles.link}>
+              <li className={styles.list}>
                 <img src="" />
                 <p>zhihnadya2002@gmail.com</p>
               </li>
